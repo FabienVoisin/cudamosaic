43705766b326c720e78f52b448b38ab5729d2581
Ubuntu <ubuntu@ip-10-0-3-80.ap-southeast-2.compute.internal>
2025-02-19 03:26:08 +0000

    modified:   src/astroio.cu 	modified:   src/astroio.h 	modified:   src/main.cu 	modified:   src/struct.cuh

--- src/main.cu
+++ src/main.cu
@@ -1,6 +1,9 @@
 #include "struct.cuh"
 #include "astroio.h"
 
+
+
+
 int main(){
     astrojpg_rgb_<Npp8u> image1("Orion/orion_1.jpg");
     image1.getgreyimage(); //get the grey image 
@@ -40,7 +43,7 @@ int main(){
 
     /*We need to load a new  image */
     std::cout<<"Am I here 2"<<std::endl;
-    astrojpg_rgb_<Npp8u> image2("Orion/orion_41.jpg");
+    astrojpg_rgb_<Npp8u> image2("Orion/orion_1.jpg");
     image2.getgreyimage();
     image2.getsignalimage(image2.nppgreyimage,threshold);
     /*Create three new images, the correlation image, the exposure map and the new combinbed image*/
@@ -50,7 +53,7 @@ int main(){
     saveastro<Npp8u,1>(image2.correlationimage,"correlationexample.jpg");
     image2.getmaxpixel(image2.correlationimage,image2.maxcorrposition,maxbuffer);
     std::cout<<"Maximum corr pixel: x="<<image2.maxcorrposition.x<<",y="<<image2.maxcorrposition.y<<std::endl;
-    astrojpg_rgb_<Npp8u> image3("Orion/orion_82.jpg");
+    astrojpg_rgb_<Npp8u> image3("Orion/orion_4.jpg");
     image3.getgreyimage();
     image3.getsignalimage(image3.nppgreyimage,threshold);
     image3.Correlationimage(image1.maskimage,sumbuffer);
@@ -60,11 +63,18 @@ int main(){
 
     /*Create a new function for mosaicing the stuff*/
     unsigned int differencex,differencey;
-    differencex=image2.maxcorrposition.x-image1.maxcorrposition.x;
-    differencey=image2.maxcorrposition.y-image1.maxcorrposition.y;
+    
     astrojpg_rgb_<Npp32f> imagetotal("Orion/orion_1.jpg");
+    imagetotal.getgreyimage();
+    imagetotal.getsignalimage(imagetotal.nppgreyimage,threshold);
+    imagetotal.Correlationimage(image1.maskimage,sumbuffer);
+    imagetotal.getmaxpixel(imagetotal.correlationimage,imagetotal.maxcorrposition,maxbuffer);
+    differencex=image3.maxcorrposition.x-imagetotal.maxcorrposition.x;
+    differencey=image3.maxcorrposition.y-imagetotal.maxcorrposition.y;
+    std::cout<<"diff="<<imagetotal.maxcorrposition.x<<","<<imagetotal.maxcorrposition.y<<std::endl;
     cv::Point_<int> offsetposition={differencex,differencey};
-    imagetotal.stackimage(image2,offsetposition);
-
+    imagetotal.stackimage(image3,offsetposition);
+    saveastro<Npp32f,3>(imagetotal.nppinputimage,"finalresult.jpg");
+    saveastro<Npp32f,1>(imagetotal.exposuremap,"finalresultexp.jpg");
     return 0;
 }
